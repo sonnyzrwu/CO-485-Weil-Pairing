32a5c94326af600599cd41c6e0cd01945514c1b7
CanIHasC00kie <82300963+CanIHasC00kie@users.noreply.github.com>
2023-01-06 00:59:17 -0500

    multivar poly multi dynamically implemented

--- src/main.c
+++ src/main.c
@@ -1,37 +1,51 @@
 #include <stdio.h>
-#include "miller.h"
-
-
-
+#include "miller.h"  
 
 int main(){
 	printf("Okay here we go... \n");
-	struct Point generator = {11, 9, 0};
-	struct EllipticCurve toy = {12, 1, 2, 2, generator}; 
-	struct Point t = {2, 1, 0};
+	//struct Point generator = {11, 9, 0};
+	// struct EllipticCurve toy = {12, 1, 1, 0, generator}; 
+	//struct Point t = {2, 1, 0};
 
-	struct Point * P = &t;
+	struct Polynomial * poly = NULL;
+	struct Polynomial * poly2 = NULL;
+	struct Polynomial * mod = NULL;
 
-	printf("Working millers algorithm... \n ");
 
-	struct RationalFunction *f = miller(5, P, toy);
 
-	int ** arr = f->P->data;
+	poly = makePolynomial(poly, 3, 3);
 
-	int row = f->P->rows;
+	poly->data[0][1] = 1;
+	poly->data[1][0] = 1;
+	poly2 = makePolynomial(poly2, 3, 3);
 
-	int col = f->P->cols;
+	poly2->data[0][1] = 1;
+	poly2->data[1][0] = 1;
 
-	for(int i = 0; i < row; ++i){
-		for(int j = 0; j < col; ++j){
-			printf("%d ",arr[i][j]);
+	mod = makePolynomial(mod, 3, 4);
+	
+	mod->data[0][0] = 1; // b
+	
+	mod->data[2][0] = -1;
+	mod->data[0][3] = 1;
+	mod->data[0][1] = 1; // a
+	
+	
+	
+	poly = multiply(poly, poly2, mod);// (x+1) (x+1)
+	print(poly->data, poly->rows, poly->cols);// 1 2 1
+	printf("Segfault after multiply? \n");
+	
+	poly = multiply(poly, poly2, mod);// (x+1)^2 * (x+1)
+	print(poly->data, poly->rows, poly->cols);
 
-		}
-		printf("\n");
-	}
 
-	destroy_rational_function(f);
+	poly = multiply(poly, poly2, mod);// 
+	print(poly->data, poly->rows, poly->cols);
+	
+	destroyPolynomial(poly);
+	destroyPolynomial(poly2);
+	destroyPolynomial(mod);
 
 	return 0;
 }
-
